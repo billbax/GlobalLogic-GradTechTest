cb7239343b186b176f7bdf174ed7edee111f37a7
Bill <billbaxter1998@gmail.com>
2022-05-27 13:02:51 +0100

    re-order results table

--- medals.py
+++ medals.py
@@ -23,10 +23,10 @@ def createMedalTable(results: list):
     # the winner 3 points, second place 2 points and third place 1 point
     points_conversion_dict = {"1": 3, "2": 2, "3": 1}
 
-    # Create an empty dict to house the nation and the cumulative score
+    # Create an empty dict to house the nation and their corresponding cumulative scores
     results_table = {}
 
-    # Iterate through each dict in the results list to get the results of each sport by accessing the 'podium' key
+    # Iterate through the results list to get the results of each sport by accessing the 'podium' key
     for sport in results:
         podium_list = sport["podium"]
 
@@ -34,18 +34,35 @@ def createMedalTable(results: list):
             # Split the results item in the podium_list using the '.' separator to get both the placing and the nation
             placing = (podium_spot.split(".")[0])
             nation = (podium_spot.split(".")[1])
-            # Convert the placing to the amount of points
+            # Convert the placing to the amount of points to be awarded based on the points_conversion_dict
             points_to_award = points_conversion_dict[placing]
 
-            # Exception handling
-            # If nation key already in results table += points based on points_to_award
+            # Use exception handling, if nation key already exists in results_table += points to the nation based on
+            # the points_to_award value.
             try:
                 results_table[nation] += points_to_award
-            # If nation not already in table, create a new dict key for the nation
+            # If key for 'nation' not already in results_table, create new dict key setting points_to_award as the value
             except KeyError:
                 results_table[nation] = points_to_award
-    print(results_table)
-    return
+
+    # reverse the order of the keys
+    nation_keys = list(results_table.keys())
+    reversed_keys = nation_keys[::-1]
+
+    # Sort the points from results table to run highest to lowest
+    sorted_values = sorted(results_table.values(), reverse=True)
+
+    sorted_dict = {}
+
+    # Re-order the results_table dict
+    for i in sorted_values:
+        for k in reversed_keys:
+            if results_table[k] == i:
+                sorted_dict[k] = results_table[k]
+
+
+
+    return sorted_dict
 
 
 def test_function():
