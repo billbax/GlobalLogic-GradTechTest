944aa927a4f80314e60b9008ea8f18ddeb8ae12b
Bill <billbaxter1998@gmail.com>
2022-05-27 12:48:08 +0100

    create initial unordered results table

--- medals.py
+++ medals.py
@@ -17,14 +17,39 @@ medalResults = [
     }
 ]
 
-def createMedalTable(results):
-    # Use the results object above to create a medal table
-    # The winner gets 3 points, second place 2 points and third place 1 point
+
+def createMedalTable(results: list):
+    # Create a placing to points conversion dict to be referenced when creating the initial results table giving
+    # the winner 3 points, second place 2 points and third place 1 point
+    points_conversion_dict = {"1": 3, "2": 2, "3": 1}
+
+    # Create an empty dict to house the nation and the cumulative score
+    results_table = {}
+
+    # Iterate through each dict in the results list to get the results of each sport by accessing the 'podium' key
+    for sport in results:
+        podium_list = sport["podium"]
+
+        for podium_spot in podium_list:
+            # Split the results item in the podium_list using the '.' separator to get both the placing and the nation
+            placing = (podium_spot.split(".")[0])
+            nation = (podium_spot.split(".")[1])
+            # Convert the placing to the amount of points
+            points_to_award = points_conversion_dict[placing]
+
+            # Exception handling
+            # If nation key already in results table += points based on points_to_award
+            try:
+                results_table[nation] += points_to_award
+            # If nation not already in table, create a new dict key for the nation
+            except KeyError:
+                results_table[nation] = points_to_award
+    print(results_table)
     return
 
 
 def test_function():
-    #This it the test function, please don't change me
+    # This it the test function, please don't change me
     medalTable = createMedalTable(medalResults)
     expectedTable = {
         "Italy": 4,
@@ -38,3 +63,6 @@ def test_function():
         "Belarus": 1,
     }
     assert medalTable == expectedTable
+
+
+createMedalTable(medalResults)
